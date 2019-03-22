9321d30eac24a67121d659883e71fd123d37a2ca
Saarthak <saarthakgupta08@gmail.com>
2019-03-22 11:55:53 +0530

    Added dependencies & updated project

--- app/src/main/java/com/example/e_bookrecordkeeper/MainActivity.java
+++ app/src/main/java/com/example/e_bookrecordkeeper/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.e_bookrecordkeeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
