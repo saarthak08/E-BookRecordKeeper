8abd8d3f3d3c0f75e1115417d98e686bb944a7ed
Saarthak <saarthakgupta08@gmail.com>
2019-03-24 13:06:35 +0530

    Added RecyclerView for bookslist of specific categories

---
+++ app/src/main/java/com/example/e_bookrecordkeeper/BooksAdapter.java
@@ -0,0 +1,75 @@
+package com.example.e_bookrecordkeeper;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.e_bookrecordkeeper.databinding.BookListItemBinding;
+import com.example.e_bookrecordkeeper.model.Book;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BookViewHolder>{
+    private OnItemClickListener listener;
+    private ArrayList<Book> books=new ArrayList<>();
+
+
+
+    @NonNull
+    @Override
+    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        BookListItemBinding bookListItemBinding= DataBindingUtil.inflate(LayoutInflater.from(viewGroup.getContext()),
+                R.layout.book_list_item,viewGroup,false);
+        return new BookViewHolder(bookListItemBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookViewHolder bookViewHolder, int i) {
+        Book book=books.get(i);
+        bookViewHolder.bookListItemBinding.setBook(book);
+    }
+
+    @Override
+    public int getItemCount() {
+        return books.size();
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+        notifyDataSetChanged();
+    }
+
+    class BookViewHolder extends RecyclerView.ViewHolder{
+        private BookListItemBinding bookListItemBinding;
+
+        public BookViewHolder(@NonNull BookListItemBinding bookListItemBinding) {
+            super(bookListItemBinding.getRoot());
+            this.bookListItemBinding=bookListItemBinding;
+            bookListItemBinding.getRoot().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int clickedPosition=getAdapterPosition();
+
+                    if(listener!=null && clickedPosition!=RecyclerView.NO_POSITION) {
+                        listener.onItemClick(books.get(clickedPosition));
+                    }
+                }
+            });
+
+        }
+
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(Book book);
+    }
+
+    public void setListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+}
--- app/src/main/java/com/example/e_bookrecordkeeper/MainActivity.java
+++ app/src/main/java/com/example/e_bookrecordkeeper/MainActivity.java
@@ -18,17 +18,24 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
     private MainActivityViewModel viewModel;
     private ActivityMainBinding activityMainBinding;
     private Category selectedCategory;
     private ArrayList<Category> categoriesList;
+    private ArrayList<Book> bookArrayList;
+    private RecyclerView booksRecyclerView;
+    private BooksAdapter booksAdapter;
+
 
 
     @Override
@@ -39,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         activityMainBinding= DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
         activityMainBinding.setClickHandlers(new MainActivityClickHandler());
-
         FloatingActionButton fab = findViewById(R.id.fab);
         viewModel= ViewModelProviders.of(MainActivity.this).get(MainActivityViewModel.class);
         viewModel.getAllCategories().observe(MainActivity.this, new Observer<List<Category>>() {
@@ -54,24 +60,12 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        viewModel.getAllBooksOfACategory(3).observe(MainActivity.this, new Observer<List<Book>>() {
-            @Override
-            public void onChanged(List<Book> books) {
-                for(Book b: books)
-                {
-                    Log.i("MyTAG",b.getBookName());
-                }
-            }
-        });
-
     }
 
     private void showOnSpinner(){
-
         ArrayAdapter<Category> categoryArrayAdapter=new ArrayAdapter<Category>(this,R.layout.spinner_item,categoriesList);
         categoryArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
         activityMainBinding.setAndroidSpinner(categoryArrayAdapter);
-
     }
 
     public class MainActivityClickHandler
@@ -80,18 +74,30 @@ public class MainActivity extends AppCompatActivity {
         {
             Toast.makeText(getApplicationContext(),"FAB Button Clicked",Toast.LENGTH_LONG).show();
         }
-
         public void onSelectItem(AdapterView<?> parent, View view, int pos, long id) {
-
             selectedCategory = (Category) parent.getItemAtPosition(pos);
-
-            String message = " id is " + selectedCategory.getId() + "\n name is " + selectedCategory.getCategoryName() + "\n email is " + selectedCategory.getCategoryDescription();
-
-            // Showing selected spinner item
-            Toast.makeText(parent.getContext(), message, Toast.LENGTH_LONG).show();
-
-
+            loadBooksArrayList(selectedCategory.getId());
         }
+    }
+
+    private void loadBooksArrayList(int categoryId){
+        viewModel.getAllBooksOfACategory(categoryId).observe(this, new Observer<List<Book>>() {
+            @Override
+            public void onChanged(@Nullable List<Book> books) {
+                bookArrayList=(ArrayList<Book>) books;
+                loadRecyclerView();
+            }
+        });
+
+    }
+
+    private void loadRecyclerView() {
+        booksRecyclerView = activityMainBinding.secondaryLayout.rvBooks;
+        booksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        booksRecyclerView.setHasFixedSize(true);
+        booksAdapter = new BooksAdapter();
+        booksRecyclerView.setAdapter(booksAdapter);
+        booksAdapter.setBooks(bookArrayList);
     }
 
     @Override
@@ -107,12 +113,10 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
